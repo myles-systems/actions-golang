7230cb8d2bbb005f7a2e7b1be7843a0fda35b0ad
Paul Vollmer <paul.vollmer@protonmail.com>
2019-02-06 08:52:16 +0100

    Removed fmt and lint args to be more flexible

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,4 @@ APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
 
 mkdir -p ${APP_DIR} && cp -r ./ ${APP_DIR} && cd ${APP_DIR}
 
-if [[ "$1" == "fmt" ]]; then
-  go fmt ./...
-fi
-
-if [[ "$1" == "lint" ]]; then
-  golint ./...
-fi
+sh -c "$*"
